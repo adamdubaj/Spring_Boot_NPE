d0ccd908c3247036f4b2b7a9be7f6311c663aa0e
adamd <adamdubaj1@gmail.com>
2023-11-05 12:11:48 +0100

    Null cases fix

--- src/main/java/com/adamdubaj/spring_boot_npe/controller/PostController.java
+++ src/main/java/com/adamdubaj/spring_boot_npe/controller/PostController.java
@@ -20,15 +20,17 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public List<PostDto> getPosts(@RequestParam(required = false) int page, Sort.Direction sort) {
-        int pageNumber = page >= 0 ? page : 0;
-        return PostDtoMapper.mapToPostDtos(postService.getPosts(pageNumber, sort));
+    public List<PostDto> getPosts(@RequestParam(required = false) Integer page, Sort.Direction sort) {
+        int pageNumber = page != null && page >= 0 ? page : 0;
+        Sort.Direction sortDirection = sort != null ? sort : Sort.Direction.ASC;
+        return PostDtoMapper.mapToPostDtos(postService.getPosts(pageNumber, sortDirection));
     }
 
     @GetMapping("/posts/comments")
-    public List<Post> getPostsWithComments(@RequestParam(required = false) int page, Sort.Direction sort) {
-        int pageNumber = page >= 0 ? page : 0;
-        return postService.getPostsWithComments(pageNumber, sort);
+    public List<Post> getPostsWithComments(@RequestParam(required = false) Integer page, Sort.Direction sort) {
+        int pageNumber = page != null && page >= 0 ? page : 0;
+        Sort.Direction sortDirection = sort != null ? sort : Sort.Direction.ASC;
+        return postService.getPostsWithComments(pageNumber, sortDirection);
     }
 
     @GetMapping("/posts/{id}")
